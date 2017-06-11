712d0bf390e5de31549d4a85dc005401acdb83a9
tanishi109 <hayakawa@goodpatch.com>
2017-06-11 18:42:30 +0900

    :tada: Now button pressed states are available.

--- src/components/GamepadPlayground/index.tsx
+++ src/components/GamepadPlayground/index.tsx
@@ -2,16 +2,49 @@ import * as React from "react";
 import {Component} from "react";
 import {RouteComponentProps} from "react-router";
 
+import GamepadManager from "./GamepadManager";
+
 interface Prop extends RouteComponentProps<{}> {}
-interface State {}
+interface State {
+  buttons: GamepadButton[];
+}
 
 class GamepadPlayground extends Component<Prop, State> {
+  constructor() {
+    super();
+
+    this.state = {
+      buttons: [],
+    };
+  }
+
+  componentDidMount() {
+    new GamepadManager((gamepad: Gamepad) => { // tslint:disable-line no-unused-expression
+      this.setState({
+        buttons: gamepad.buttons,
+      });
+    });
+  }
+
   render() {
     return (
       <div>
-        welcome to playground!
+        {this.renderButtons()}
       </div>
     );
+  }
+
+  private renderButtons() {
+    const {buttons} = this.state;
+
+    return buttons.map((btn, i) => {
+      return (
+        <div key={i}>
+          <h4>{`btn_${i}`}</h4>
+          <span>pressed: {btn.pressed + ""}</span>
+        </div>
+      );
+    });
   }
 }
 
