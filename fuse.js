f6d118c8199a2e51d25ca293d8ab13d9bacb3cb3
tanishi109 <hayakawa@goodpatch.com>
2017-06-11 13:33:13 +0900

    :+1: dev server is required for routing

--- fuse.js
+++ fuse.js
@@ -11,7 +11,24 @@ const fuse = FuseBox.init({
   output: "dist/$name.js",
 });
 
-fuse.dev();
+const path = require("path");
+const express = require("express");
+
+fuse.dev({ root: false }, server => {
+  const dist = path.resolve("./dist");
+  const app = server.httpServer.app;
+
+  app.use("/static/", express.static(path.join(dist, "static")));
+
+  app.get("/app.js", function(req, res) {
+    res.sendFile(path.join(dist, "app.js"));
+  });
+
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(dist, "index.html"));
+  });
+});
+
 fuse
   .bundle("app")
   .watch().hmr()
