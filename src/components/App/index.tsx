227f08acd6e38911f522bec3865b7b31e58df3e6
tanishi109 <hayakawa@goodpatch.com>
2017-06-18 11:29:49 +0900

    :tada: controll the cube by JoyCon(L)!

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -4,6 +4,7 @@ import {RouteComponentProps} from "react-router";
 import * as THREE from "three";
 
 import Vars from "./Vars";
+import GamepadManager from "../GamepadPlayground/GamepadManager";
 
 const init = () => {
   const scene = new THREE.Scene();
@@ -28,40 +29,87 @@ const init = () => {
   Vars.camera = camera;
 };
 
-const animate = () => {
-  requestAnimationFrame( animate );
+const datGui = () => {
+  const gui = new dat.GUI();
+  const camera = gui.addFolder("camera.position");
 
-  const {renderer, scene, camera} = Vars;
+  camera.add(Vars.camera.position, "x", -500, 500);
+  camera.add(Vars.camera.position, "y", -500, 500);
+  camera.add(Vars.camera.position, "z", 0, 1000);
+  camera.open();
 
-  renderer.render( scene, camera );
+  const rotation = gui.addFolder("camera.rotation");
+
+  rotation.add(Vars.camera.rotation, "x", -2, 2, 0.01);
+  rotation.add(Vars.camera.rotation, "y", -2, 2, 0.01);
+  rotation.add(Vars.camera.rotation, "z", -2, 2, 0.01);
+  rotation.open();
 };
 
 interface Prop extends RouteComponentProps<{}> {}
-interface State {}
+interface State {
+  vy: number;
+  vx: number;
+  animateLoop: () => void;
+}
 
 class App extends Component<Prop, State> {
+  constructor() {
+    super();
+
+    this.state = {
+      vy: 0,
+      vx: 0,
+      animateLoop: () => {
+        const {renderer, scene, camera, mesh} = Vars;
+        const {vx, vy} = this.state;
+
+        mesh.rotation.x += vx;
+        mesh.rotation.y += vy;
+
+        renderer.render( scene, camera );
+      },
+    };
+  }
 
   componentDidMount() {
     init();
-    animate();
+    this.animate.bind(this)();
+    datGui();
 
-    const gui = new dat.GUI();
-    const camera = gui.addFolder("camera");
+    new GamepadManager((gamepad: Gamepad) => { // tslint:disable-line no-unused-expression
+      const axes = gamepad.axes[gamepad.axes.length - 1].toFixed(4);
+      const vymap = {
+        "0.7143": -0.02,
+        "-0.4286": 0.02,
+      };
+      const vxmap = {
+        "-1.0000": 0.02,
+        "0.1429": -0.02,
+      };
 
-    camera.add(Vars.camera.position, "x", -500, 500);
-    camera.add(Vars.camera.position, "y", -500, 500);
-    camera.add(Vars.camera.position, "z", 0, 1000);
-    camera.open();
-
-    const rotation = gui.addFolder("rotation");
-
-    rotation.add(Vars.camera.rotation, "x", -2, 2, 0.01);
-    rotation.add(Vars.camera.rotation, "y", -2, 2, 0.01);
-    rotation.add(Vars.camera.rotation, "z", -2, 2, 0.01);
-    rotation.open();
+      if (vymap[axes] || vxmap[axes]) {
+        this.setState({
+          vy: vymap[axes] || 0,
+          vx: vxmap[axes] || 0,
+        });
+      } else {
+        this.setState({
+          vy: 0,
+          vx: 0,
+        });
+      }
+    });
   }
 
-  render() {
+  animate() {
+    const {animateLoop} = this.state;
+
+    animateLoop();
+    requestAnimationFrame(this.animate.bind(this));
+  }
+
+  render(): JSX.Element {
     return (
       <div>
       </div>
